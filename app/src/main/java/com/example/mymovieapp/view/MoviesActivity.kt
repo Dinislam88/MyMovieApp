a9769285780b7bc9e1f2d0989650dfacda165635
Dinislam88 <dinis.sib88@gmail.com>
2023-02-24 23:46:52 +0500

    Making Movies Activity and adding RecyclerView

---
+++ app/src/main/java/com/example/mymovieapp/view/MoviesActivity.kt
@@ -0,0 +1,50 @@
+package com.example.mymovieapp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesActivity : AppCompatActivity(), CustomAdapter.ItemClickListener {
+
+    private val mViewModel: MoviesViewModel = MoviesViewModel()
+
+    private lateinit var mMoviesRecycler: RecyclerView
+    private lateinit var mMoviesAdapter: CustomAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+        initViews()
+        initObservers()
+        mViewModel.getMovies()
+
+    }
+
+    private fun initObservers() {
+        mViewModel.apply {
+            movies.observe(this@MoviesActivity) {
+                mMoviesAdapter = CustomAdapter(it, this@MoviesActivity)
+                mMoviesRecycler.adapter = mMoviesAdapter
+            }
+        }
+    }
+
+    private fun initViews() {
+        mMoviesRecycler = findViewById<RecyclerView>(R.id.recyclerview)
+        mMoviesRecycler.layoutManager = GridLayoutManager(this, 2)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        this.finishAffinity()
+    }
+
+    override fun onItemClick(position: Int) {
+        val intent = Intent(this@MoviesActivity, MovieDetailsActivity::class.java)
+        intent.putExtra("id",position)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/mymovieapp/view/adapters/CustomAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.mymovieapp.view.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val mList: List<Result?>,
+val mItemClickListener: MoviesActivity
+) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Picasso.get().load("https://image.tmdb.org/t/p/w500/" + mList[position]?.poster_path).into(holder.imageView)
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    inner class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageview)
+        init {
+            ItemView.setOnClickListener {
+                mList[position]?.id?.let { it -> mItemClickListener.onItemClick(it) }
+            }
+        }
+    }
+}
